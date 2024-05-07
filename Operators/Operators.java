01f13a706f2de9c8eae2c28f7ed197bf6fe4c5dc
Ajeypalsingh <ajeypals7@gmail.com>
2024-05-06 19:00:35 -0500

    operators in java

---
+++ Operators/Operators.java
@@ -0,0 +1,47 @@
+package Java.Operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        /*
+         
+        Operator -> Symbols that tell compiler to perform some operation or calculation
+
+        Arithmatic operators -> operates two variables ex. a + b here + is operator adding a and b
+             + -> add variables
+             - -> subtract variables
+             * -> product variables
+             / -> Divide variables
+             % -> give remainder
+
+             ++ -> increment number by one
+             -- -> decrement number by one
+
+             ++a -> preincrement - first change value then use value
+             a++ -> postincrement - first use value then change value
+
+
+             Relational operators - to compare variables these operators return boolean value
+
+               == -> for checking wheather two digits are equal
+               != -> for checking wheather two digits are not equal
+               > and < -> checks two variable greater or lesser than other
+               => and <= -> checks two variable greater or equal to other and lesser or equal to other
+
+            Logical operators - compares two conditions or statements it also returns boolean value
+              
+              && -> return true only if two statements are true
+              || -> return true if atlreast one statement is true
+              !  -> switch true to false and vice versa
+
+            Bitwise operator
+              
+             &  -> returns true if two bits are 1 and false if two bits are 0 or different
+             |  -> returns true if two bits are different otherwise returns false
+             ^  -> returns false if two bits are same and true with same bits
+             ~ -> it switch the bit from true to false and vice versa
+             << -> it shifts all bits to left to some space
+             >> -> it shifts all bits to right to some space    
+        */
+
+    }
+}
